ac3e28c5ab0dd7fb0512371827321f087d390f5d
Bruno Bastos Neves <brunoniconeves@gmail.com>
2021-09-15 21:22:22 -0300

    Add navbar  and remove trash code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <navbar/>
+    <b-alert show dismissible> {{ msg }}! </b-alert>
+    <b-list-group class="mt-5">
+      <b-list-group-item v-for="item in items" v-bind:key="item.id">
+      {{ item.message }}
+      <b-badge>{{item.versions}}</b-badge>
+      </b-list-group-item>
+    </b-list-group>
+    <div class="text-center m-5">
+      <b-button variant="primary" size="sm" v-on:click="share_count += 1">
+        Share on Twitter <b-badge variant="light">{{ share_count }}</b-badge>
+      </b-button>
+    </div>
   </div>
-  <router-view />
 </template>
+
+<script>
+import navbar from './components/navbar/navbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    navbar
+  },
+  data () {
+    return {
+      msg: 'Isso é um teste',
+      items: [
+      { message: 'Nokia 8', versions: 5 },
+      { message: 'OnePlus 5T' },
+      { message: 'Samsung Galaxy S9' }
+    ],
+      share_count:0,
+      teste: 'Teste'
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+h1, h2 {
+  font-weight: normal;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
 }
 </style>
---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">   
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" v-for="item in menuItems" v-bind:key="item.id" :class="{ active: item.isActive }">{{item.text}}</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">         
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">PT</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      menuItems: [
+        { text: 'Quem sou eu', isActive: true},
+        { text: 'Trabalhos', isActive: false },
+        { text: 'Contato', isActive: false }
+      ],  
+    }
+  }
+}
+</script>
+
